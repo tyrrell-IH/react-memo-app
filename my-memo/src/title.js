60cbe3a8f6e663fea8c989ba2925ed0a7c1450e1
tyrrell-IH <gotothertm@gmail.com>
2024-12-22 10:31:28 +0900

    state変数'editingMemo'がnullの時にtitleが表示されない不具合を修正

--- my-memo/src/title.js
+++ my-memo/src/title.js
@@ -2,7 +2,7 @@ export default function Title({ changeEditMode, editingMemo, setEditingMemo }) {
   const memos = JSON.parse(localStorage.getItem("memos")) ?? [];
   const titles = memos.map((memo) => {
     const title = memo.body.split("\n")[0];
-    if (memo.id === editingMemo.id) {
+    if (editingMemo !== null && memo.id === editingMemo.id) {
       return <li key={memo.id}>{title}</li>;
     }
     return (
