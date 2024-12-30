512cc8da837aefa7bd07bf2a7b9a4e8b301081d8
tyrrell-IH <gotothertm@gmail.com>
2024-12-31 00:02:55 +0900

    早期returnを使っていた箇所を、if/else文に修正

--- my-memo/src/title_index.js
+++ my-memo/src/title_index.js
@@ -5,19 +5,20 @@ export default function TitleIndex({ editingMemo, setEditingMemo, allMemos }) {
   const titles = allMemos.map((memo) => {
     if (editingMemo !== null && memo.id === editingMemo.id) {
       return <li key={memo.id}>{memo.title}</li>;
+    } else {
+      return (
+        <li key={memo.id}>
+          <a
+            href="#"
+            onClick={() => {
+              setEditingMemo(memo);
+            }}
+          >
+            {memo.title}
+          </a>
+        </li>
+      );
     }
-    return (
-      <li key={memo.id}>
-        <a
-          href="#"
-          onClick={() => {
-            setEditingMemo(memo);
-          }}
-        >
-          {memo.title}
-        </a>
-      </li>
-    );
   });
 
   return (
